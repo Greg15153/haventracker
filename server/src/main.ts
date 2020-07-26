861a566bb94bfb50bb22b397231670f250d68020
Greg15153 <4452658+Greg15153@users.noreply.github.com>
2020-07-26 00:46:51 -0400

    Added helmet to Fastify for security headers

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,5 @@
+import * as helmet from 'fastify-helmet'
+
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
 import { AppModule } from './app.module'
@@ -5,6 +7,8 @@ import { NestFactory } from '@nestjs/core'
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+
+    app.register(helmet)
     app.enableCors()
     await app.listen(5000)
 }
